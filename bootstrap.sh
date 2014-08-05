c9e900b5c6361c9a0ac1d90f94aa32b96d79e8f1
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-08-05 14:13:22 +0200

    Added bootstrap script.

---
+++ bootstrap.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# add mirrors for faster download
+cat >/tmp/sources.list <<EOL
+deb mirror://mirrors.ubuntu.com/mirrors.txt precise main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt precise-updates main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt precise-backports main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt precise-security main restricted universe multiverse
+EOL
+
+cat /etc/apt/sources.list >> /tmp/sources.list
+mv /tmp/sources.list /etc/apt/sources.list
+
+# make sure we are not asked any questions
+export DEBIAN_FRONTEND=noninteractive
+
+# update repository information
+apt-get -y update
+
+# prepare answers for prompts
+echo mysql-server-5.5 mysql-server/root_password password caracal | debconf-set-selections
+echo mysql-server-5.5 mysql-server/root_password_again password caracal | debconf-set-selections
+echo phpmyadmin phpmyadmin/dbconfig-install boolean true | debconf-set-selections
+echo phpmyadmin phpmyadmin/mysql/admin-pass password caracal | debconf-set-selections
+echo phpmyadmin phpmyadmin/mysql/app-pass password caracal | debconf-set-selections
+echo phpmyadmin phpmyadmin/app-password-confirm password caracal | debconf-set-selections
+echo phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2 | debconf-set-selections
+
+# install database server first
+apt-get -y install mysql-server
+
+# install the remaining part of the server stack
+apt-get -y install phpmyadmin libapache2-mod-php5 php5-curl git
+
+# clone caracal repository
+cd /var/www
+rm -Rf *
+git clone https://github.com/Way2CU/Caracal.git .
+git checkout develop
+
+# link directories
+# ln -s /var/www/data /vagrant/data
+# ln -s /var/www/styles /vagrant/styles
+# ln -s /var/www/scripts /vagrant/scripts
+# ln -s /var/www/site /vagrant/site
