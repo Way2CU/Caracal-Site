ce5a6e75340c7679e93fd529cd5d345ecd939482
Mladen Mijatov <meaneye.rcf@gmail.com>
2015-05-14 18:39:51 +0200

    Main script: Create mobile menu.

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -49,6 +49,8 @@ Site.is_mobile = function() {
  * Function called when document and images have been completely loaded.
  */
 Site.on_load = function() {
+	if (Site.is_mobile())
+		Site.mobile_menu = new Caracal.MobileMenu();
 };
 
 
