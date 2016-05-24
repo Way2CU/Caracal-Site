6c9866cffc76eeee8b2cd89494b18a86edb1142a
Mladen Mijatov <meaneye.rcf@gmail.com>
2016-05-24 15:17:55 +0300

    Deployment script: Create PID file.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 cd deploy
+PID_FILE=/tmp/ansible.pid
 
 if [ -f "hosts.txt" ]; then
-	ansible-playbook --limit @hosts.txt deploy.yml
+	ansible-playbook --limit @hosts.txt deploy.yml &
+
+	echo $! > $PID_FILE
+	wait $!
+	rm -f $PID_FILE
+	exit $?
+
 else
 	echo 'Missing "hosts" file.'
 fi
