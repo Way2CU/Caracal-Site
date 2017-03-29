7ad795123687109d6b9ea63e59faa5ebf866ae05
Mladen Mijatov <meaneye.rcf@gmail.com>
2017-03-29 11:46:59 +0300

    Config: Changed session type value.

--- site/config.php
+++ site/config.php
@@ -21,10 +21,9 @@ $available_languages = array('en');
 $default_language = 'en';
 
 // default session options
-$session_type = Session::TYPE_BROWSER;
+$session_type = SessionType::BROWSER;
 
 // database
-$db_use = true;
 $db_type = DatabaseType::MYSQL;
 $db_config = array(
 		'host' => 'localhost',
